b896c2413f9f48a8a304b0155745a00d2d25e6e8
Vitaliy Makogon <mvitaliyd@gmail.com>
2019-03-12 10:42:55 +0200

    update

--- scripts/ci/update-pkg.js
+++ scripts/ci/update-pkg.js
@@ -8,12 +8,31 @@ updatePkg();
 
 async function updatePkg() {
   const packageJson = JSON.parse(fs.readFileSync(pathToJSONFile), 'utf8');
-  const time = (new Date).getTime();
 
-  packageJson.version = `0.0.${time}`;
+  packageJson.version = `${packageJson.version}-dev.${getTime()}`;
   packageJson.name = NGX_BOOTSRAP_CI;
 
   fs.writeFile(pathToJSONFile, JSON.stringify(packageJson, null, 2));
 
   console.log(`Package.json was updated`);
+}
+
+function getTime() {
+  const date = new Date();
+  let month = '' + (date.getMonth() + 1);
+  let day = '' + date.getDate();
+  const year = date.getFullYear();
+  const hour = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  if (month.length < 2) {
+    month = '0' + month;
+  }
+
+  if (day.length < 2) {
+    day = '0' + day;
+  }
+
+  return `${year}${month}${day}${hour}${minutes}${seconds}`;
 }
